acab50e19e54b66b1de631f27e81c1a30cee678c
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-01-15 08:14:53 +0300

    Working on files that comes

--- src/components/Header/NaveBar.jsx
+++ src/components/Header/NaveBar.jsx
@@ -1,5 +1,73 @@
 const NaveBar = () => {
-  return <div>NaveBar</div>;
+  return (
+    <div className="nav-wrapper fixed-top">
+      <div className="container">
+        <nav className="navbar navbar-toggleable-sm navbar-expand-md">
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target=".navbar-collapse"
+          >
+            ☰
+          </button>
+          <a className="navbar-brand mx-auto" href="#">
+            <img src="images/icons/logo-sm.png" />
+          </a>
+
+          <div className="navbar-collapse collapse">
+            <ul className="navbar-nav nav-justified w-100 nav-fill">
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="/mac/">
+                  Mac
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  iphone
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  ipad
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  watch
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  tv
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  Music
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#">
+                  Support
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="/search/">
+                  <img src="images/icons/search-icon-sm.png" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="/cart/">
+                  <img src="images/icons/cart-sm.png" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default NaveBar;
