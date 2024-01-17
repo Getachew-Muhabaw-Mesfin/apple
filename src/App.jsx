61392301b0a1dae63bda86af52ea1fd9b424b82d
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-01-17 09:01:26 +0300

    React slick slider

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NaveBar from "./components/Header/NaveBar";
 import Footer from "./components/Footer/Footer";
 import Highlight from "./components/Highlights/Highlights";
 import GridProduct from "./components/Products/GridProduct";
-
+import Carousels from "./components/Slider/Carousels";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <NaveBar />
       <Highlight />
       <GridProduct />
+      <Carousels />
       <Footer />
     </>
   );
