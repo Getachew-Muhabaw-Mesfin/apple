50f488f303e290c5b9919f6699313d3b1331e54b
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-01-17 08:00:06 +0300

    Grid product is Done

--- src/components/Products/GridProduct.jsx
+++ src/components/Products/GridProduct.jsx
@@ -1,148 +1,76 @@
 import Link from "../Link/Link";
-import Watch9 from "../../assets/images/logos/promo_logo_apple_watch_series_9_large.png";
-import WatchUltera from "../../assets/images/logos/promo_logo_apple_watch_ultra2__large.png";
-import MacBook from "../../assets/images/logos/mackbook_log.jpg";
-import Fitness from "../../assets/images/logos/promo_logo_fitness_large.png";
-import ApplCard from "../../assets/images/logos/logo_card_large.png";
-import TradeIn from "../../assets/../assets/images/logos/logo_tradein__large.png";
-import "./GridProduct.css";
-
-const promos = [
-  {
-    id: 1,
-    icon: Watch9,
-    description: "Smart. Brighter.Mightier",
-    bgImage: "apple_watch9_series",
-  },
-  {
-    id: 2,
-    icon: WatchUltera,
-    description: "Next level adventure",
-    bgImage: "watchUltra2",
-  },
-  {
-    id: 3,
-    icon: MacBook,
-    description: "Mind-blowing. Head-turning",
-    bgImage: "mackBookPro",
-  },
-  {
-    id: 4,
-    icon: Fitness,
-    description:
-      "From HIIT to Yoga. From 5 to 45 minutes. There's Something For everyone",
-    bgImage: "fitness",
-  },
-  {
-    id: 5,
-    icon: ApplCard,
-    description: "Get up to 3% Daily Cash back with every Purchase",
-    bgImage: "appleCard",
-  },
-  {
-    id: 6,
-    icon: TradeIn,
-    description:
-      "Get $180-$620 in credit when you trade in iPhone 11 or Higher.",
-    bgImage: "tradein",
-  },
-];
-
+import products from "./produt_data";
 const GridProduct = () => {
+  //TODO: Add style on Describtion 
   return (
-    <div className="m">
-      <div className="row">
-        {promos.map((product, id) => (
-          <div key={id} className={`${product.bgImage} col-sm-12 col-md-6`}>
-            <div className="right-side-container">
-              <div className="top-logo-wrapper">
-                <div className="logo-wrapper">
-                  <img src={product.icon} alt="logo" />
-                </div>
-              </div>
-              <div className="description-wraper">
-                {product.description}
-                <br />
-              </div>
-              <Link />
-            </div>
+    <div className="row">
+      {products.map((product, id) => (
+        <div
+          key={id}
+          className={`${product.bgImageWithStyle} col-sm-12 col-md-6`}
+        >
+          <div className="text-center">
+            <img src={product.icon} alt="logo" />
           </div>
-        ))}
-      </div>
+          <div className="text-center">
+            {product.description}
+            <br />
+          </div>
+          <Link />
+        </div>
+      ))}
     </div>
   );
 };
 
 export default GridProduct;
 
-// import Link from "../Link/Link";
+//////////////////////////Reference incase if not working  ////////////////////////
 // import Watch9 from "../../assets/images/logos/promo_logo_apple_watch_series_9_large.png";
 // import WatchUltera from "../../assets/images/logos/promo_logo_apple_watch_ultra2__large.png";
 // import MacBook from "../../assets/images/logos/mackbook_log.jpg";
 // import Fitness from "../../assets/images/logos/promo_logo_fitness_large.png";
 // import ApplCard from "../../assets/images/logos/logo_card_large.png";
 // import TradeIn from "../../assets/../assets/images/logos/logo_tradein__large.png";
+// import "./GridProduct.css";
+
 // const promos = [
 //   {
 //     id: 1,
 //     icon: Watch9,
 //     description: "Smart. Brighter.Mightier",
-//     bgImage: "",
+//     bgImage: "apple_watch9_series commen-style",
 //   },
 //   {
 //     id: 2,
 //     icon: WatchUltera,
 //     description: "Next level adventure",
-//     bgImage: "",
+//     bgImage: "watchUltra2 commen-style",
 //   },
 //   {
 //     id: 3,
 //     icon: MacBook,
 //     description: "Mind-blowing. Head-turning",
-//     bgImage: "",
+//     bgImage: "mackBookPro commen-style",
 //   },
 //   {
 //     id: 4,
 //     icon: Fitness,
 //     description:
-//       "From HIIT to Yoga. From 5 to 45 minutes. There's Something Form everyone",
-//     bgImage: "",
+//       "From HIIT to Yoga. From 5 to 45 minutes. There's Something For everyone",
+//     bgImage: "fitness commen-style",
 //   },
 //   {
 //     id: 5,
 //     icon: ApplCard,
 //     description: "Get up to 3% Daily Cash back with every Purchase",
-//     bgImage: "",
+//     bgImage: "appleCard commen-style",
 //   },
 //   {
 //     id: 6,
 //     icon: TradeIn,
 //     description:
 //       "Get $180-$620 in credit when you trade in iPhone 11 or Higher.",
-//     bgImage: "",
+//     bgImage: "tradein commen-style",
 //   },
 // ];
-// console.log(promos);
-// const GridProduct = () => {
-//   return promos.map((product, id) => (
-//     <div
-//       key={id}
-//       className={`right-side-wrapper ${product.bgImage} col-sm-12 col-md-6`}
-//     >
-//       <div className="right-side-container">
-//         <div className="top-logo-wrapper">
-//           <div className="logo-wrapper">
-//             <img src={product.icon} />
-//           </div>
-//         </div>
-//         <div className="description-wraper">
-//           {product.description}
-//           <br />
-//         </div>
-//         <Link />
-//       </div>
-//     </div>
-//   ));
-// };
-
-// export default GridProduct;
---
+++ src/components/Products/produt_data.js
@@ -0,0 +1,48 @@
+import Watch9 from "../../assets/images/logos/promo_logo_apple_watch_series_9_large.png";
+import WatchUltera from "../../assets/images/logos/promo_logo_apple_watch_ultra2__large.png";
+import MacBook from "../../assets/images/logos/mackbook_log.jpg";
+import Fitness from "../../assets/images/logos/promo_logo_fitness_large.png";
+import ApplCard from "../../assets/images/logos/logo_card_large.png";
+import TradeIn from "../../assets/../assets/images/logos/logo_tradein__large.png";
+import "./GridProduct.css";
+const products = [
+  {
+    id: 1,
+    icon: Watch9,
+    description: "Smart. Brighter.Mightier",
+    bgImageWithStyle: "apple_watch9_series commen-style",
+  },
+  {
+    id: 2,
+    icon: WatchUltera,
+    description: "Next level adventure",
+    bgImageWithStyle: "watchUltra2 commen-style",
+  },
+  {
+    id: 3,
+    icon: MacBook,
+    description: "Mind-blowing. Head-turning",
+    bgImageWithStyle: "mackBookPro commen-style",
+  },
+  {
+    id: 4,
+    icon: Fitness,
+    description:
+      "From HIIT to Yoga. From 5 to 45 minutes. There's Something For everyone",
+    bgImageWithStyle: "fitness commen-style",
+  },
+  {
+    id: 5,
+    icon: ApplCard,
+    description: "Get up to 3% Daily Cash back with every Purchase",
+    bgImageWithStyle: "appleCard commen-style",
+  },
+  {
+    id: 6,
+    icon: TradeIn,
+    description:
+      "Get $180-$620 in credit when you trade in iPhone 11 or Higher.",
+    bgImageWithStyle: "tradein commen-style",
+  },
+];
+export default products
