15564ee504b14db22b352a8df3ea947f5b9e5499
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-01-17 09:15:00 +0300

    working on silders

--- src/components/Slider/Carousels.jsx
+++ src/components/Slider/Carousels.jsx
@@ -2,41 +2,47 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import silderOne from "../../assets/images/sliders/slider-one.png";
+import silderTwo from "../../assets/images/sliders/slider two.png";
+import silderThree from "../../assets/images/sliders/slider-three.png";
+import silderFour from "../../assets/images/sliders/slider-four.png";
+import silderFive from "../../assets/images/sliders/slider-five.png";
+import silderSix from "../../assets/images/sliders/slider-six.png";
+import silderSeven from "../../assets/images/sliders/slider-seven.png";
+import silderEight from "../../assets/images/sliders/slider-eight.png";
+import silderNine from "../../assets/images/sliders/slider nine.png";
+import silderTen from "../../assets/images/sliders/slider-ten.png";
+
+const images = [
+  silderOne,
+  silderTwo,
+  silderThree,
+  silderFour,
+  silderFive,
+  silderSix,
+  silderSeven,
+  silderEight,
+  silderNine,
+  silderTen,
+];
 
 const Carousels = () => {
-const settings = {
-  dots: true,
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  speed: 5000,
-  autoplaySpeed: 2000,
-  cssEase: "linear",
-};
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
   return (
-    <div>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+    <Slider {...settings}>
+      {images.map((slider, index) => (
+        <div key={index}>
+          <img src={slider} alt="SilderImage" />
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
+      ))}
+    </Slider>
   );
 };
 
