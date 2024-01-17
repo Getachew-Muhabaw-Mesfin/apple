d3c2484898047535046ede66496eb3268407367c
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-01-17 09:39:18 +0300

    Separte of Concerns with images and components

--- src/components/Slider/Carousels.jsx
+++ src/components/Slider/Carousels.jsx
@@ -1,30 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-import silderOne from "../../assets/images/sliders/slider-one.png";
-import silderTwo from "../../assets/images/sliders/slider two.png";
-import silderThree from "../../assets/images/sliders/slider-three.png";
-import silderFour from "../../assets/images/sliders/slider-four.png";
-import silderFive from "../../assets/images/sliders/slider-five.png";
-import silderSix from "../../assets/images/sliders/slider-six.png";
-import silderSeven from "../../assets/images/sliders/slider-seven.png";
-import silderEight from "../../assets/images/sliders/slider-eight.png";
-import silderNine from "../../assets/images/sliders/slider nine.png";
-import silderTen from "../../assets/images/sliders/slider-ten.png";
-
-const images = [
-  silderOne,
-  silderTwo,
-  silderThree,
-  silderFour,
-  silderFive,
-  silderSix,
-  silderSeven,
-  silderEight,
-  silderNine,
-  silderTen,
-];
+import images from "./images_data";
 
 const Carousels = () => {
   const settings = {
@@ -36,7 +13,7 @@ const Carousels = () => {
     autoplaySpeed: 2000,
   };
   return (
-    <Slider {...settings}>
+    <Slider {...settings} className="">
       {images.map((slider, index) => (
         <div key={index}>
           <img src={slider} alt="SilderImage" />
---
+++ src/components/Slider/images_data.js
@@ -0,0 +1,24 @@
+import silderOne from "../../assets/images/sliders/slider-one.png";
+import silderTwo from "../../assets/images/sliders/slider two.png";
+import silderThree from "../../assets/images/sliders/slider-three.png";
+import silderFour from "../../assets/images/sliders/slider-four.png";
+import silderFive from "../../assets/images/sliders/slider-five.png";
+import silderSix from "../../assets/images/sliders/slider-six.png";
+import silderSeven from "../../assets/images/sliders/slider-seven.png";
+import silderEight from "../../assets/images/sliders/slider-eight.png";
+import silderNine from "../../assets/images/sliders/slider nine.png";
+import silderTen from "../../assets/images/sliders/slider-ten.png";
+
+const images = [
+  silderOne,
+  silderTwo,
+  silderThree,
+  silderFour,
+  silderFive,
+  silderSix,
+  silderSeven,
+  silderEight,
+  silderNine,
+  silderTen,
+];
+export default images;
