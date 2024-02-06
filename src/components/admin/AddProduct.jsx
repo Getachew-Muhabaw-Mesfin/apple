3fb8689ee8aecaa56da268a664f42b6008b01a92
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-02-06 11:54:49 +0300

    Make Forms col6

--- src/components/admin/AddProduct.jsx
+++ src/components/admin/AddProduct.jsx
@@ -62,8 +62,8 @@ function AddProduct() {
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
-            <div >
-              <div>
+            <div className="row">
+              <div className="col-md-6">
                 <Form.Group controlId="product_url">
                   <Form.Label>Product URL</Form.Label>
                   <Form.Control
@@ -75,83 +75,102 @@ function AddProduct() {
                   />
                 </Form.Group>
               </div>
-              <Form.Group controlId="product_name">
-                <Form.Label>Product Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="product_name"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="product_name">
-                <Form.Label>Starting Price</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="starting_price"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="product_name">
-                <Form.Label>Price Range</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="price_range"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="product_name">
-                <Form.Label>Brif Description</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="brief_description"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="product_name">
-                <Form.Label>Full Description</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="full_description"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="product_name">
-                <Form.Label>Product Image</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="product_img"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="product_name">
-                <Form.Label>Product Link</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter product name"
-                  name="product_link"
-                  value={formData.product_name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
+              <div className="col-md-6">
+                <Form.Group controlId="product_name">
+                  <Form.Label>Product Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="product_name"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group controlId="starting_price">
+                  <Form.Label>Starting Price</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="starting_price"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group controlId="price_range">
+                  <Form.Label>Price Range</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="price_range"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
 
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit">
-                Add Product
-              </Button>
+              <div className="col-md-6">
+                <Form.Group controlId="brief_description">
+                  <Form.Label>Brif Description</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="brief_description"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group controlId="full_description">
+                  <Form.Label>Full Description</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="full_description"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group controlId="product_img">
+                  <Form.Label>Product Image</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="product_img"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-md-6">
+                <Form.Group controlId="product_link">
+                  <Form.Label>Product Link</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter product name"
+                    name="product_link"
+                    value={formData.product_name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </div>
+
+              <div className="col-md-6">
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </div>
+              <div className="col-md-6">
+                <Button variant="primary" type="submit">
+                  Add Product
+                </Button>
+              </div>
             </div>
           </Form>
         </Modal.Body>
