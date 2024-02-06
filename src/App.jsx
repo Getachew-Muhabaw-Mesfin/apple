c2f63d4559486f69ff2483654969111609f167b3
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-02-06 14:12:13 +0300

    File and Folder Structure of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import NaveBar from "./components/Header/NaveBar";
 import Footer from "./components/Footer/Footer";
-import Highlight from "./components/Highlights/Highlights";
-import GridProducts from "./components/Products/GridProducts";
-import Carousels from "./components/Slider/Carousels";
-import Videos from "./components/Videos/Videos";
+
+// import Highlight from "./components/Highlights/Highlights";
+// import GridProducts from "./components/Products/GridProducts";
+// import Carousels from "./components/Slider/Carousels";
+// import Videos from "./components/Videos/Videos";
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
       <NaveBar />
-      <Highlight />
-      <GridProducts />
-      <Carousels />
-      <Videos/>
+      <Routes>
+        <Route path="/" element= {<h1>Home</h1>}/>
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import {
+  Highlight,
+  GridProducts,
+  Carousels,
+  Videos,
+} from "../../components";
+
+function Home() {
+  return (
+    <>
+      <Highlight />
+      <GridProducts />
+      <Carousels />
+      <Videos />
+    </>
+  );
+}
+
+export default Home;
